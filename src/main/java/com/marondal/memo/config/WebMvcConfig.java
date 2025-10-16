57ed12e52855527a7fd351074120f8fc2f3ead26
dulumary <lecture@hagulu.com>
2025-10-16 21:35:45 +0900

    [김인규]  - 권한 인터셉터 추가

--- src/main/java/com/marondal/memo/config/WebMvcConfig.java
+++ src/main/java/com/marondal/memo/config/WebMvcConfig.java
@@ -1,7 +1,9 @@
 package com.marondal.memo.config;
 
 import com.marondal.memo.common.FileManager;
+import com.marondal.memo.interceptor.PermissionInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,6 +14,15 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/images/**")
                 .addResourceLocations("file:///" + FileManager.FILE_UPLOAD_PATH + "/");
+
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+        registry.addInterceptor(new PermissionInterceptor())
+                .addPathPatterns("/**")
+                .excludePathPatterns("/user/logout", "/static/**", "/images/**");
 
     }
 }
